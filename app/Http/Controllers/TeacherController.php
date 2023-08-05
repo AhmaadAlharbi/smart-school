825140f385b0a8ce2d4e1b7baae3ed615436015f
ahmad <ahmad@ahmads-iMac.local>
2023-08-05 03:09:12 +0300

    update and delete teachers

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -36,4 +36,46 @@ class TeacherController extends Controller
             return redirect()->back()->with(['error' => $e->getMessage()]);
         }
     }
+    public function edit(Teacher $teacher)
+    {
+        if (!$teacher) {
+            abort(404);
+        }
+        $specializations = Specialization::all();
+        $genders = Gender::all();
+        return view('pages.teachers.edit', compact('teacher', 'specializations', 'genders'));
+    }
+    public function update(Request $request, $id)
+    {
+        // Validate the incoming request data
+        $request->validate([
+            'Name_ar' => 'required|string|max:255',
+            'Name_en' => 'required|string|max:255',
+            'Specialization_id' => 'required|numeric',
+            'Gender_id' => 'required|numeric',
+            'Joining_Date' => 'required|date',
+            'Address' => 'nullable|string',
+        ]);
+
+        // Find the teacher to be updated
+        $teacher = Teacher::findOrFail($id);
+
+        // Update the teacher's data
+        $teacher->setTranslation('Name', 'ar', $request->input('Name_ar'));
+        $teacher->setTranslation('Name', 'en', $request->input('Name_en'));
+        $teacher->Specialization_id = $request->input('Specialization_id');
+        $teacher->Gender_id = $request->input('Gender_id');
+        $teacher->Joining_Date = $request->input('Joining_Date');
+        $teacher->Address = $request->input('Address');
+        $teacher->save();
+
+        // Redirect back to the teachers list with a success message
+        return redirect()->route('teachers.index')->with('success', 'Teacher updated successfully.');
+    }
+    public function destroy(Request $request)
+    {
+        Teacher::findOrFail($request->id)->delete();
+        toastr()->success(trans('messages.success'));
+        return back()->withInput(['grade' => $request->id]);
+    }
 }
--- resources/views/pages/teachers/edit.blade.php
+++ resources/views/pages/teachers/edit.blade.php
@@ -43,24 +43,24 @@
             <div class="col-span-12">
                 <div class="box">
                     <div class="box-body">
-                        <form action="{{route('teachers.store')}}" method="post"
+                        <form action="{{route('teachers.update', $teacher->id)}}" method="post"
                             class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 grid gird-cols-1 md:grid-cols-2 gap-4">
                             @csrf
-
-
-
+                            @method('PUT')
                             <div class="form-row mb-4">
                                 <div class="col">
                                     <label for="name_ar"
                                         class="block text-gray-700 text-sm mb-2">{{trans('Teacher_trans.Name_ar')}}</label>
                                     <input type="text" name="Name_ar" class="form-control ti-form-input"
-                                        placeholder="{{trans('Teacher_trans.Name_ar')}}">
+                                        placeholder="{{trans('Teacher_trans.Name_ar')}}"
+                                        value="{{ $teacher->getTranslation('Name','ar') }}">
                                 </div>
                                 <div class="col">
                                     <label for="name_en"
                                         class="block text-gray-700 text-sm mb-2">{{trans('Teacher_trans.Name_en')}}</label>
                                     <input type="text" name="Name_en" class="form-control ti-form-input"
-                                        placeholder="{{trans('Teacher_trans.Name_en')}}">
+                                        placeholder="{{trans('Teacher_trans.Name_en')}}"
+                                        value="{{ $teacher->getTranslation('Name','en') }}">
                                 </div>
                             </div>
 
@@ -71,7 +71,9 @@
                                     <select class="custom-select ti-form-select" name="Specialization_id">
                                         <option selected disabled>{{trans('Parent_trans.Choose')}}...</option>
                                         @foreach($specializations as $specialization)
-                                        <option value="{{$specialization->id}}">{{$specialization->Name}}</option>
+                                        <option value="{{$specialization->id}}" {{ $specialization->id ==
+                                            $teacher->Specialization_id ? 'selected' : ''}}>{{$specialization->Name}}
+                                        </option>
                                         @endforeach
                                     </select>
                                 </div>
@@ -81,7 +83,8 @@
                                     <select class="custom-select ti-form-select" name="Gender_id">
                                         <option selected disabled>{{trans('Parent_trans.Choose')}}...</option>
                                         @foreach($genders as $gender)
-                                        <option value="{{$gender->id}}">{{$gender->Name}}</option>
+                                        <option value="{{$gender->id}}" {{ $gender->id == $teacher->Gender_id ?
+                                            'selected' : ''}}>{{$gender->Name}}</option>
                                         @endforeach
                                     </select>
                                 </div>
@@ -94,7 +97,8 @@
                                     <input type="text" name="Joining_Date"
                                         class="ti-form-input rounded-l-none focus:z-10 flatpickr-input"
                                         placeholder="{{trans('Teacher_trans.Joining_Date')}}" id="date"
-                                        placeholder="Choose date">
+                                        placeholder="Choose date" value="{{ $teacher->Joining_Date }}">
+
                                 </div>
                             </div>
 
@@ -102,12 +106,14 @@
                                 <label for="address"
                                     class="block text-gray-700 text-sm mb-2">{{trans('Teacher_trans.Address')}}</label>
                                 <textarea class="form-control ti-form-input" name="Address" id="address"
-                                    rows="4"></textarea>
+                                    rows="4">{{ $teacher->Address }}</textarea>
                             </div>
 
                             <button class="ti-btn ti-btn-primary"
-                                type="submit">{{trans('main_trans.add_teacher')}}</button </div>
+                                type="submit">{{trans('main_trans.edit_teacher')}}</button>
+                        </form>
                     </div>
+
                 </div>
             </div>
             <!-- End::row-1 -->
@@ -128,4 +134,5 @@
     <!-- COLOR PICKER JS -->
     <script src="{{asset('build/assets/libs/@simonwep/pickr/pickr.es5.min.js')}}"></script>
     @vite('resources/assets/js/colorpicker.js')
+
     @endsection
